9c92c215736ab27f528cc921bef3aba19cb0de44
Sankalp Shrivastava <90796151+sankalP010@users.noreply.github.com>
2023-10-10 21:54:36 +0530

    Updated Solution - GfG to GitHub

--- Easy/Max Min/max-min.java
+++ Easy/Max Min/max-min.java
@@ -41,14 +41,10 @@ class Solution
     {
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
-        
-        for (int i = 0; i < N; i++) {
-            if(A[i] < min) {
-                min = A[i];
-            }
-            if(A[i] > max) {
-                max = A[i];
-            }
+        for(int i = 0; i < N; i++) {
+            
+            min = Math.min(min, A[i]);
+            max = Math.max(max, A[i]);
         }
         return min + max;
     }
